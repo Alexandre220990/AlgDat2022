224475c5959e53d5ddf766b71956b93c3699266e
Alexandre220990 <drike.alexandre22@gmail.com>
2022-02-03 17:43:12 +0000

    new

--- AlgDatTest.java
+++ AlgDatTest.java
@@ -2,7 +2,7 @@ public class AlgDatTest {
     public static void main(String[] args) {
         encription enc = new encription();
 
-        //System.out.println(enc.encryptRowColumn("this is a secret message",10));
+        System.out.println(enc.encryptRowColumn("this is a secret message",10));
 
 
 
